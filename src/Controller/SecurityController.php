d1e94cc407c618e3f6682564a781126be4b41e9f
Theo <t.enfedaque@lprs.fr>
2025-04-30 05:50:11 +0200

    feat : redirection

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -48,13 +48,17 @@ final class SecurityController extends AbstractController
         $error = $authenticationUtils->getLastAuthenticationError();
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
+        
+
 
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
 
     #[Route(path: '/logout', name: 'app_logout')]
-    public function logout(): void
+    public function logout(): Response
     {
-        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+        //throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+        return new Response("test");
+
     }
 }
